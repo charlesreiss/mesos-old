4abf85b046d8da988ee4fbd6d54137556f405ed5
Charles Reiss <charles@eecs.berkeley.edu>
2012-02-03 17:09:56 -0800

    Better fake::Scenario tests.

--- src/tests/fake/scenario_test.cpp
+++ src/tests/fake/scenario_test.cpp
@@ -7,28 +7,67 @@ using namespace mesos;
 using namespace mesos::internal;
 using namespace mesos::internal::fake;
 
-TEST(FakeScenarioTest, SampleBatch) {
-  process::Clock::pause();
-  Scenario scenario;
-  scenario.spawnMaster();
-  for (int i = 0; i < 4; ++i) {
-    scenario.spawnSlave(Resources::parse("cpu:4;mem:1024"));
-  }
-  std::map<TaskID, FakeTask*> tasks;
-  for (int i = 0; i < 16; ++i) {
+class FakeScenarioTest : public testing::Test
+{
+protected:
+  void makeBatchTask(int i, std::map<TaskID, FakeTask*>* tasks) {
     TaskID id;
     id.set_value(std::string("task") + boost::lexical_cast<std::string>(i));
-    tasks[id] = new BatchTask(Resources::parse("mem:512"),
-                              ResourceHints::parse("mem:512;cpu:1",
-                                                   "mem:512;cpu:1"),
-                              30.0, 3.0);
+    (*tasks)[id] = new BatchTask(Resources::parse("mem:512"),
+                                 ResourceHints::parse("mem:512;cpus:1",
+                                                      ""),
+                                 30.0, 3.0);
   }
-  scenario.spawnScheduler("batch", tasks);
-  scenario.finishSetup();
-  EXPECT_EQ(16, scenario.getScheduler("batch")->countPending());
-  scenario.runFor(16.0 * 30.0 / 4.0 + 0.1);
-  EXPECT_EQ(0, scenario.getScheduler("batch")->countPending());
-  EXPECT_EQ(0, scenario.getScheduler("batch")->countRunning());
-  scenario.stop();
-  process::Clock::resume();
+
+  void batchTest(int numSlaves, int numTasks, double time) {
+    process::Clock::pause();
+    scenario.spawnMaster();
+    for (int i = 0; i < numSlaves; ++i) {
+      scenario.spawnSlave(Resources::parse("cpus:4;mem:1024"));
+    }
+    std::map<TaskID, FakeTask*> tasks;
+    for (int i = 0; i < numTasks; ++i) {
+      makeBatchTask(i, &tasks);
+    }
+    scenario.spawnScheduler("batch", tasks);
+    scenario.finishSetup();
+    scenario.runFor(time);
+    EXPECT_EQ(0, scenario.getScheduler("batch")->countPending());
+    EXPECT_EQ(0, scenario.getScheduler("batch")->countRunning());
+    scenario.stop();
+    process::Clock::resume();
+  }
+  Scenario scenario;
+};
+
+// These tests assume that the isolation policy prevents tasks from getting
+// "free" CPU cycles.
+TEST_F(FakeScenarioTest, OneTaskBatch)
+{
+  batchTest(1, 1, 30.1);
+}
+
+TEST_F(FakeScenarioTest, OneTaskTwoSlavesBatch)
+{
+  batchTest(2, 1, 30.1);
+}
+
+TEST_F(FakeScenarioTest, TwoTasksOneSlaveBatch)
+{
+  batchTest(1, 2, 30.1);
+}
+
+TEST_F(FakeScenarioTest, ThreeTasksOneSlaveBatch)
+{
+  batchTest(1, 3, 60.1);
+}
+
+TEST_F(FakeScenarioTest, ThreeTasksTwoSlavesBatch)
+{
+  batchTest(2, 3, 30.1);
+}
+
+TEST_F(FakeScenarioTest, FiveTasksTwoSlavesBatch)
+{
+  batchTest(2, 3, 60.1);
 }
