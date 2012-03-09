b98f40b2441bb270c872f774a2f46dff1a702722
Charles Reiss <charles@eecs.berkeley.edu>
2012-03-08 21:23:46 -0800

    make-scenario.sh tweaks.

--- simulate-scenarios/make-scenario.sh
+++ simulate-scenarios/make-scenario.sh
@@ -1,3 +1,27 @@
+MAKE_SCENARIO='python ../src/fake/make_scenario.py --repeat=20'
+
+if false; then
+    $MAKE_SCENARIO --vary_memory_round --num_background=2 --slaves=1 \
+  >vary_memory_round_noexp_2.json
+
+$MAKE_SCENARIO --vary_memory_round --num_background=3 --slaves=1 \
+  >vary_memory_round_noexp_3.json
+
+$MAKE_SCENARIO --vary_memory_round --num_background=4 --slaves=1 \
+  >vary_memory_round_noexp_4.json
+
+fi
+
+$MAKE_SCENARIO --vary_memory_round --num_background=4 --slaves=10 \
+  --target_memory_seconds=20000 \
+  >vary_memory_round_noexp_long.json
+
+$MAKE_SCENARIO --vary_memory_round --num_background=40 --slaves=10 \
+  --target_memory_seconds=4000 --interarrival=400 \
+  >vary_memory_round_interarrive_long.json
+
+exit
+
 
 REPEAT=50
 #python ../src/fake/make_scenario.py \
@@ -7,9 +31,19 @@ python ../src/fake/make_scenario.py --repeat=$REPEAT \
 python ../src/fake/make_scenario.py --repeat=$REPEAT \
   --vary_memory --experiment_memory=5 --stretch_time=10 --cpu_request=2 >vary_mem_simple_stretch.json
 
+REPEAT=5
 python ../src/fake/make_scenario.py --repeat=$REPEAT \
   --vary_memory --experiment_memory=5 --memory_accuracy=0.1 --cpu_request=2 \
   >vary_mem_simple_acc.json
 python ../src/fake/make_scenario.py --repeat=$REPEAT \
   --vary_memory --experiment_memory=5 --memory_accuracy=0.1 --stretch_time=10 \
   --cpu_request=2 >vary_mem_simple_stretch_acc.json
+
+REPEAT=5
+$MAKE_SCENARIO --repeat=$REPEAT --num_background=0 --slaves=1 \
+  --vary_memory > vary_mem_nobackground.json
+
+REPEAT=5
+$MAKE_SCENARIO --repeat=$REPEAT --num_background=0 --slaves=4 \
+  --vary_memory > vary_mem_nobackground4.json
+
--- simulate-scenarios/run-scenarios.sh
+++ simulate-scenarios/run-scenarios.sh
@@ -2,18 +2,31 @@
 
 set -e
 
-SIMULATE=../build-linux/src/mesos-simulate
+VALGRIND='../build-linux/libtool --mode=execute valgrind --tool=cachegrind'
+SIMULATE='../build-linux/src/mesos-simulate --fake_interval=0.015625'
 SCENARIO=$1
 
 export GLOG_minloglevel=1
 #export GLOG_v=2
 
-mv $1 $1.old.$(date +%s) || true
+if test -d $1; then
+  mv $1 old/$1.old.$(date +%s)
+fi
 mkdir $1
+mkdir $1/logs
 
 $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
-  --allocator=norequest >$SCENARIO/norequest.csv &
+  --allocator=norequest --usage_log_base=$SCENARIO/logs/norequest. \
+  >$SCENARIO/norequest.csv &
+if false; then
+    $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
+      --allocator=norequest --norequest_aggressive \
+      >$SCENARIO/norequest-aggressive.csv &
+fi
 $SIMULATE --json_file=$SCENARIO.json --allocator=simple \
+  --usage_log_base=$SCENARIO/logs/simple-strong. \
   > $SCENARIO/simple-strong.csv &
-$SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
-  --allocator=simple >$SCENARIO/simple-weak.csv &
+if false; then
+    $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
+      --allocator=simple >$SCENARIO/simple-weak.csv &
+fi
