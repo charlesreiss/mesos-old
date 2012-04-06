471677f8791ce74f7c60b340795313b60e1ce4f5
Charles Reiss <charles@eecs.berkeley.edu>
2012-04-05 17:39:37 -0700

    Tweak run-scenarios.sh script.

--- simulate-scenarios/run-scenarios.sh
+++ simulate-scenarios/run-scenarios.sh
@@ -6,12 +6,13 @@ renice -n +20 $$
 
 ulimit -c unlimited
 
-VALGRIND='../build-linux/libtool --mode=execute valgrind --tool=callgrind --cache-sim=yes'
+#VALGRIND='../build-linux/libtool --mode=execute valgrind --tool=callgrind --cache-sim=yes'
+VALGRIND='../build-linux/libtool --mode=execute valgrind --log-file=valgrind.out --suppressions=../build-linux/src/suppressions.valgrind --track-origins=yes'
 #VALGRIND=''
-SIMULATE='../build-linux/src/mesos-simulate --fake_interval=0.25'
+SIMULATE='../build-linux/src/mesos-simulate --fake_interval=0.25 --no_create_work_dir'
 
 export GLOG_minloglevel=1
-#export GLOG_v=2
+#export GLOG_v=1
 #export GLOG_logtostderr=1
 
 for SCENARIO in $@; do
@@ -34,10 +35,10 @@ for SCENARIO in $@; do
     if test x$GLOG_logtostderr != x1; then
         $SIMULATE --json_file=$SCENARIO.json --allocator=simple \
           --usage_log_base=$SCENARIO/logs/simple-strong. \
-          > $SCENARIO/simple-strong.csv &
+          > $SCENARIO/simple-strong.csv 2>$SCENARIO.logfile.ss&
         $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
           --usage_log_base=$SCENARIO/logs/simple-weak. \
-          --allocator=simple >$SCENARIO/simple-weak.csv &
+          --allocator=simple >$SCENARIO/simple-weak.csv 2>$SCENARIO.logfile.sw &
     fi
 
 done
