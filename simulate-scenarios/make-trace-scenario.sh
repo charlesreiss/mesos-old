8e30c85e6386f58b5eaca43e85c7add65481737e
Charles Reiss <charles@eecs.berkeley.edu>
2012-05-16 14:22:35 -0700

    Scenario generation tweaks.

--- simulate-scenarios/make-trace-scenario.sh
+++ simulate-scenarios/make-trace-scenario.sh
@@ -7,25 +7,162 @@ $MAKE_SCENARIO --priority=2 --priority=3 \
                --priority=4 --priority=5 --priority=6 --priority=7 \
                --priority=8 \
                --scheduling_class=0 --scheduling_class=1 \
-               --max_jobs=20 --slave_count=10 >trace_20_s10.json
+               --max_jobs=1 --slave_count=1 \
+               --min_finish_ratio=0 --min_tasks=1 >trace_k0_1_s1.json
 
 $MAKE_SCENARIO --priority=2 --priority=3 \
                --priority=4 --priority=5 --priority=6 --priority=7 \
                --priority=8 \
                --scheduling_class=0 --scheduling_class=1 \
-               --max_jobs=20 --slave_count=15 >trace_20_s15.json
+               --max_jobs=10 --slave_count=2 \
+               --min_finish_ratio=0 --min_tasks=1 >trace_k0_5_s1.json
 
 $MAKE_SCENARIO --priority=2 --priority=3 \
                --priority=4 --priority=5 --priority=6 --priority=7 \
                --priority=8 \
                --scheduling_class=0 --scheduling_class=1 \
-               --max_jobs=40 --slave_count=15 >trace_40_s15.json
+               --max_jobs=100 --slave_count=500 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_100_s500.json
 
 $MAKE_SCENARIO --priority=2 --priority=3 \
                --priority=4 --priority=5 --priority=6 --priority=7 \
                --priority=8 \
                --scheduling_class=0 --scheduling_class=1 \
-               --max_jobs=60 --slave_count=10 >trace_60_s10.json
+               --max_jobs=500 --slave_count=500 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_200_s1000.json
+
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=500 --slave_count=1000 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_200_s500.json
+
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=500 --slave_count=500 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_500_s500.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=100 \
+               --min_finish_ratio=0 --min_tasks=2 >trace_k1_100_s100.json
+if false; then
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=25 --slave_count=100 \
+               --min_finish_ratio=0 --min_tasks=2 >trace_k1_25_s100.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=25 --slave_count=2 \
+               --min_finish_ratio=0 \
+               --preserve_one_task=false >trace_k_25_s2.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=25 --slave_count=100 \
+               --min_finish_ratio=0 >trace_k_25_s100.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=25 --slave_count=4000 \
+               --min_finish_ratio=0 >trace_k_25_s4000.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=100 \
+               --min_finish_ratio=0 >trace_k_100_s100.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=100 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_100_s100.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=50 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_100_s50.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=150 \
+               --min_finish_ratio=0 \
+               --task_split=false >trace_nosplit_k_100_s150.json
+
+fi
+if false; then
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=50 --slave_count=200 >trace_50_s200.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=200 >trace_100_s200.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=400 >trace_100_s400.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=100 --slave_count=800 >trace_100_s800.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=200 --slave_count=200 >trace_200_s200.json
+
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=200 --slave_count=800 >trace_200_s800.json
+
+$MAKE_SCENARIO --priority=2 --priority=3 \
+               --priority=4 --priority=5 --priority=6 --priority=7 \
+               --priority=8 \
+               --scheduling_class=0 --scheduling_class=1 \
+               --max_jobs=800 --slave_count=1600 >trace_800_s1600.json
+fi
 
 if false; then
 $MAKE_SCENARIO --priority=2 --priority=3 \
--- simulate-scenarios/run-scenarios.sh
+++ simulate-scenarios/run-scenarios.sh
@@ -6,10 +6,10 @@ renice -n +20 $$
 
 ulimit -c unlimited
 
-#VALGRIND='../build-linux/libtool --mode=execute valgrind --tool=callgrind --cache-sim=yes'
-VALGRIND='../build-linux/libtool --mode=execute valgrind --log-file=valgrind.out --suppressions=../build-linux/src/suppressions.valgrind --track-origins=yes'
+VALGRIND='../build-linux/libtool --mode=execute valgrind --tool=callgrind --cache-sim=yes'
+#VALGRIND='../build-linux/libtool --mode=execute valgrind --log-file=valgrind.out --suppressions=../build-linux/src/suppressions.valgrind --track-origins=yes'
 #VALGRIND=''
-SIMULATE='../build-linux/src/mesos-simulate --fake_interval=0.25 --no_create_work_dir'
+SIMULATE='../build-linux/src/mesos-simulate --fake_interval=0.5 --no_create_work_dir --norequest_usage_reoffer_delay=0.001'
 
 export GLOG_minloglevel=1
 #export GLOG_v=1
@@ -23,9 +23,11 @@ for SCENARIO in $@; do
     mkdir $SCENARIO
     mkdir $SCENARIO/logs
 
-    $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
-      --allocator=norequest --usage_log_base=$SCENARIO/logs/norequest. \
-      >$SCENARIO/norequest.csv 2>$SCENARIO.logfile &
+    if test x$GLOG_logtostderr != x1; then
+        $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
+          --allocator=norequest --usage_log_base=$SCENARIO/logs/norequest. \
+          >$SCENARIO/norequest.csv 2>$SCENARIO.logfile &
+    fi
     if false; then
         $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
           --allocator=norequest --usage_log_base=$SCENARIO/logs/rorequest-aggressive. \
@@ -38,9 +40,9 @@ for SCENARIO in $@; do
           --usage_log_base=$SCENARIO/logs/simple-strong. \
           > $SCENARIO/simple-strong.csv 2>$SCENARIO.logfile.ss&
       fi
+    fi
         $SIMULATE --json_file=$SCENARIO.json --fake_extra_cpu --fake_extra_mem \
           --usage_log_base=$SCENARIO/logs/simple-weak. \
           --allocator=simple >$SCENARIO/simple-weak.csv 2>$SCENARIO.logfile.sw &
-    fi
 
 done
